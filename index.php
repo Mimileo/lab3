fab44f920db28fd8b75ecd6f3816b3284cbf49ca
acano <acano@csumb.edu>
2017-09-13 20:51:39 +0000

    adding base from challenge2

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Cards </title>
+        <meta charset ="utf-8"/>
+        <style>
+            @import url("css/styles.css");
+        </style>
+    </head>
+    <h1>
+        Computer Card Game
+    </h1>
+    <h2>
+        human computer
+    </h2>
+    <body>
+        
+        <?php
+        include 'functions.php';
+        play();
+        ?>
+        
+    
+        
+        
+    </body>
+</html>
