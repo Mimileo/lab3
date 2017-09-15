488aa8cd7c5e205cce42aef081efc35d30f97a44
acano <acano@csumb.edu>
2017-09-15 20:06:27 +0000

    adding display of card

--- functions.php
+++ functions.php
@@ -1,59 +1,29 @@
 
-        <?php
-        function displayCard($randomValue,$randomValue2){
-            
-            switch($randomValue){
-                case 0:$deck= "clubs";
-                        break;
-                case 1:$deck= "diamonds";
-                        break;
-                case 2: $deck= "hearts";
-                        break;
-                case 3:$deck= "spades";
-                        break;
+<?php
 
-            }
-             switch($randomValue2){
-                case 0:$symbol= "ten";
-                        break;
-                case 1:$symbol= "jack";
-                        break;
-                case 2: $symbol= "queen";
-                        break;
-                case 3:$symbol= "king";
-                        break;
-                case 4:$symbol= "ace";
-                        break;
-            }
-            echo "<img src='img/cards/$deck/$symbol.png' alt='$symbol'/>";
-        }
-        
-    function play(){
-        echo "<div id='move'>";
-        $card1;
-        $card2;
-        for($i =1;$i<=2;$i++){
-            $randomValue = rand(0,3);
-            $randomValue2 = rand(0,4);
-            ${"card" . $i } = $randomValue2;
-            displayCard($randomValue,$randomValue2);
-            
-        }
-        echo "<div id='win'>";
-         if($card1 > $card2)
-            {
-                echo "Human Wins";
-            }
-            else if($card1==$card2){
-                echo "Tied";
-            }
-            else{
-                echo "Computer Wins";
-            }
-           echo "</div>";
-         echo "</div>";
+    $deck = array();
+    $suits = array("clubs","spades","hearts","diamonds");
 
+for ($i = 0; $i < 4; $i++) {
+    for ($j = 1; $j <= 13; $j++) {
+    
+        $deck[] = $suits[$i] . "_" . $j;
     }
+    
+}
 
-        ?>
+foreach ($deck as $card) {
+    echo $card . "<br />";
+}
+
+foreach ($deck as $card) {
+    $stringParts = explode("_", $card);
+
+$firstPart  = $stringParts[0]; 
+$secondPart = $stringParts[1]; 
+echo "<img src='../lab3/cards/$firstPart/$secondPart.png' alt='$firstPart'/>";
+}
+
+        
+?>
         
--- index.php
+++ index.php
@@ -4,20 +4,17 @@
         <title> Cards </title>
         <meta charset ="utf-8"/>
         <style>
-            @import url("css/styles.css");
+            @import url("css/style.css");
         </style>
     </head>
     <h1>
         Computer Card Game
     </h1>
-    <h2>
-        human computer
-    </h2>
+
     <body>
         
         <?php
         include 'functions.php';
-        play();
         ?>
         
     
