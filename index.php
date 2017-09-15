ad0113c26b3b755fbd9570d428cb9c7817e58e24
eirkrw <ering-walters@csumb.edu>
2017-09-15 20:35:50 +0000

    changed index layout.

--- index.php
+++ index.php
@@ -3,24 +3,52 @@
     <head>
         <title> Cards </title>
         <meta charset ="utf-8"/>
+        <link href="./css/style.css" rel="stylesheet" type="text/css">
         <style>
-            @import url("css/styles.css");
         </style>
     </head>
-    <h1>
-        Computer Card Game
-    </h1>
-    <h2>
-        human computer
-    </h2>
+   
     <body>
+        <h1>Silver Jack</h1>
+        <table>
+        <tr>
+            <th>Player</th>
+        <th>Cards</th></tr>
+        <tr>
+            <td>
+        <div class="player" id="player1">test1</div>
+        </td>
         
+        <td> card goes here</td>
+        </tr>
+        <tr>
+            <td>
+        <div class="player" id="player2">test2</div>
+        </td>
+        <td> card goes here</td>
+        </tr>
+        
+        <tr>
+            <td>
+        <div class="player" id="player3">test3</div>
+        </td>
+        
+        <td> card goes here</td>
+        </tr>
+        <tr>
+            <td>
+        <div class="player" id="player4">test4</div>
+        </td>
+        
+        <td> card goes here</td>
+        </tr>
+        </table>
         <?php
         include 'functions.php';
         play();
         ?>
         
-    
+        
         
         
     </body>
