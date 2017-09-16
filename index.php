7be1710475cc357d462203c44ec701e36a8d323d
Mimi Leon <mleon@csumb.edu>
2017-09-16 23:28:18 +0000

    Fixed: Draw until 42

--- index.php
+++ index.php
@@ -3,7 +3,13 @@
     <head>
         <title> Cards </title>
         <meta charset ="utf-8"/>
+        <link href="./css/style.css" rel="stylesheet" type="text/css">
         <style>
+<<<<<<< HEAD
+        </style>
+    </head>
+   
+=======
             @import url("css/style.css");
         </style>
     </head>
@@ -11,14 +17,48 @@
         Computer Card Game
     </h1>
 
+>>>>>>> 33514f1d666f0cb3c309e02fdca24d32861d181a
     <body>
+        <h1>Silver Jack</h1>
+        <table>
+        <tr>
+            <th>Player</th>
+        <th>Cards</th></tr>
+        <tr>
+            <td>
+        <div class="player" id="player1">test1</div>
+        </td>
         
+        <td> card goes here</td>
+        </tr>
+        <tr>
+            <td>
+        <div class="player" id="player2">test2</div>
+        </td>
+        <td> card goes here</td>
+        </tr>
+        
+        <tr>
+            <td>
+        <div class="player" id="player3">test3</div>
+        </td>
+        
+        <td> card goes here</td>
+        </tr>
+        <tr>
+            <td>
+        <div class="player" id="player4">test4</div>
+        </td>
+        
+        <td> card goes here</td>
+        </tr>
+        </table>
         <?php
         include 'functions.php';
         playerToll();
         ?>
         
-    
+        
         
         
     </body>
