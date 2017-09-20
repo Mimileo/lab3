e8a3d2169cbd84b9d408abe4344fb4c4759c3c5f
Mimi Leon <mleon@csumb.edu>
2017-09-20 18:13:13 +0000

    Display winners and scores when tie

--- functions.php
+++ functions.php
@@ -8,17 +8,6 @@
     $player2 = array();
     $player3 = array();
     $player4 = array();
-   
-   $player5 = array(
-       // pics
-       array(),
-       // names
-       array(),
-       array()
-       );
-   
-   
-    $picsNames = array("pic1" => "name1", "pic2" => "name2", "pic3" => "3", "pic4" => "name4");
 
 for ($i = 0; $i < 4; $i++) {
     for ($j = 1; $j <= 13; $j++) {
@@ -69,23 +58,15 @@ function playerToll(){
     global $playerscore;
      $pics = array("pic1","pic2","pic3", "pic4");
      $names = array("name1", "name2", "name3", "name4");
-     //$picNames=array("pic1" => "n1","pic2"=>"n2","pic3"=> "n3","pic4"=>"n4");
-    
-     //echo "<img src='img/$pics[0].png' width='70'>";
-  
-     //echo $names[0] . "<img src='img/$pics[0].png' width='80>";
+     $winner = array();
+
      $ar = array(0,1,2,3);
      shuffle($ar);
     for($i = 1;$i<=4;$i++) {
                 $k=$ar[$i-1];  
                 echo "<img src='img/$pics[$k].png' width='70'>" . $names[$k];
                 ${"player" . $i } = player();
-                //$k = array_search('n'. $i, $sArr);
-                //echo $sArr['pic' . $i] . " <img src='img/$k.png' width='50px'/>";
-                //echo $ar[$i-1];
-               
-            
-                echo "${'player' . $i }";
+                echo " ${'player' . $i } ";
                 $playerScore[] = ${'player' . $i};
                 print_r($playerScore);
                 if(${'player' . $i } == 42) {
@@ -101,20 +82,26 @@ function playerToll(){
 
 function displayWinner($playerScore) {
    // if($playerScore ==)
-   
+   $winner=array();
     $max = 0;
     $index = 0;
     $tie = 0;
     for($i=0;$i<4;$i++) {
         if($playerScore[$i] > $max && $playerScore[$i] <= 42  ) {
             
-            $max = $playerScore[$i];
+             $max = $playerScore[$i];
+             $index = $i+1;
             //echo $max;
-            $index = $i+1;
+           
         }
        
     }
+  
     for($j=0;$j<4;$j++) {
+          if($playerScore[$j] == $max){
+              array_push($winner, $j+1);
+              print_r($winner);
+          }
         if($j == $index-1)
         continue;
         else { //&& $playerScore[$index-1] != $playerScore[$j]
@@ -126,7 +113,10 @@ function displayWinner($playerScore) {
     
     $points = array_sum($playerScore) - $playerScore[$index-1];
     if($tie > 0 || $index == 0) {
-        echo "Tie!";
+        echo "Tie!<br>";
+        for($i=0;$i<count($winner);$i++){
+            echo "Player ". $winner[$i] . " wins " . $points . " points!<br>";
+        }
     }
     else {
         echo "Player ". $index . " wins " . $points . " points!";
