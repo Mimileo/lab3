e865ac36278538a34571b8ca91068abf066b0dc5
Joel Casillas <jcasillas@csumb.edu>
2017-09-22 19:22:31 +0000

    commiting

--- index.php
+++ index.php
@@ -13,49 +13,41 @@
     </h1>
     <body>
         <h1>Silver Jack</h1>
-        <table>
-        <tr>
-            <th>Player</th>
-        <th>Cards<br><br></th></tr>
-        <tr>
-            <br>
-            <br>
-            <td>
+      
         <div class="player" id="player1"> <img src="player1.jpg"  height="175" width="150" alt="Picture of a baby boy">
-</div>
-        </td>
         
-        <td> <?php
-        include 'functions.php';
-        playerToll(0);
-        ?>
-        </tr>
-        <tr>
-            <td>
+</div>
+    
+        
+        
+
         <div class="player" id="player2"> <img src="player1.jpg"  height="175" width="150" alt="Picture of a baby boy">
 </div>
-        </td>
+      
         
-        </tr>
+    
         
-        <tr>
-            <td>
+      
+           
         <div class="player" id="player3"> <img src="player1.jpg"  height="175" width="150" alt="Picture of a baby boy">
 </div>
-        </td>
+      
         
        
-        </tr>
-        <tr>
-            <td>
+       
+       
+          
         <div class="player" id="player4"> <img src="player1.jpg"  height="175" width="150" alt="Picture of a baby boy">
 </div>
-        </td>
         
-        </td>
-        </tr>
-        </table>
-        
+     <div class="work">
+        <center>
+        <?php
+        include 'functions.php';
+        playerToll();
+        ?>
+        </center>
+        </div>
         
         
         
