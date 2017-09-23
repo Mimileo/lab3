f0d2d583ddedb543803f811b67afbc84890db264
Joel Casillas <jcasillas@csumb.edu>
2017-09-23 17:14:52 +0000

    deleting mario virus

--- index.php
+++ index.php
@@ -19,31 +19,6 @@
         </font>
 
       
-        <div class="player" id="player1"> <img src="player1.jpg"  height="175" width="150" alt="Picture of a baby boy">
-        
-</div>
-    
-        
-        
-
-        <div class="player" id="player2"> <img src="player1.jpg"  height="175" width="150" alt="Picture of a baby boy">
-</div>
-      
-        
-    
-        
-      
-           
-        <div class="player" id="player3"> <img src="player1.jpg"  height="175" width="150" alt="Picture of a baby boy">
-</div>
-      
-        
-       
-       
-       
-          
-        <div class="player" id="player4"> <img src="player1.jpg"  height="175" width="150" alt="Picture of a baby boy">
-</div>
         
      <div class="work">
         <center>
