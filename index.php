00b211fffc3fc4569d362f8c8c9d7c2c655ff446
acano <acano@csumb.edu>
2017-09-23 16:44:59 +0000

    backUp

--- index.php
+++ index.php
@@ -12,90 +12,11 @@
   
     <body>
         <h1>Silver Jack</h1>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-        <!--<table>-->
-        <!--<tr>-->
-        <!--    <th>Player</th>-->
-        <!--<th>Cards</th></tr>-->
-        <!--<tr>-->
-        <!--    <td>-->
-        <!--<div class="player" id="player1">test1</div>-->
-        <!--</td>-->
-=======
-<<<<<<< HEAD
-      
-        <div class="player" id="player1"> <img src="player1.jpg"  height="175" width="150" alt="Picture of a baby boy">
-        
-</div>
-    
-        
-        
 
-        <div class="player" id="player2"> <img src="player1.jpg"  height="175" width="150" alt="Picture of a baby boy">
-</div>
-      
-        
-    
-        
-      
-           
-        <div class="player" id="player3"> <img src="player1.jpg"  height="175" width="150" alt="Picture of a baby boy">
-</div>
-      
-        
-       
-       
-       
-          
-        <div class="player" id="player4"> <img src="player1.jpg"  height="175" width="150" alt="Picture of a baby boy">
+ 
 </div>
         
-     <div class="work">
-        <center>
-        <?php
-        include 'functions.php';
-        playerToll();
-        ?>
-        </center>
-        </div>
-=======
->>>>>>> 875864bb266fb806e80119de2553101229bda84b
-        <table>
-        <tr>
-            <th>Player</th>
-        <th>Cards</th></tr>
-        <tr>
-            <td>
-        <div class="player" id="player1">test1</div>
-        </td>
->>>>>>> d8624ace643bc6d2846976579eaf8e087b36a309
         
-        <!--<td> card goes here</td>-->
-        <!--</tr>-->
-        <!--<tr>-->
-        <!--    <td>-->
-        <!--<div class="player" id="player2">test2</div>-->
-        <!--</td>-->
-        <!--<td> card goes here</td>-->
-        <!--</tr>-->
-        
-        <!--<tr>-->
-        <!--    <td>-->
-        <!--<div class="player" id="player3">test3</div>-->
-        <!--</td>-->
-        
-        <!--<td> card goes here</td>-->
-        <!--</tr>-->
-        <!--<tr>-->
-        <!--    <td>-->
-        <!--<div class="player" id="player4">test4</div>-->
-        <!--</td>-->
-        
-        <!--<td> card goes here</td>-->
-        <!--</tr>-->
-        <!--</table>-->
         <?php
         include 'functions.php';
         playerToll();
