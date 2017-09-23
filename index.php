92d4b7fa7611682a72a3cce9c8948b039947f8dd
eirkrw <ering-walters@csumb.edu>
2017-09-23 00:43:43 +0000

    changed img size and removed table

--- index.php
+++ index.php
@@ -9,44 +9,42 @@
             @import url("css/style.css");
         </style>
     </head>
-    <h1>
-        Computer Card Game
-    </h1>
+  
     <body>
         <h1>Silver Jack</h1>
-        <table>
-        <tr>
-            <th>Player</th>
-        <th>Cards</th></tr>
-        <tr>
-            <td>
-        <div class="player" id="player1">test1</div>
-        </td>
+        <!--<table>-->
+        <!--<tr>-->
+        <!--    <th>Player</th>-->
+        <!--<th>Cards</th></tr>-->
+        <!--<tr>-->
+        <!--    <td>-->
+        <!--<div class="player" id="player1">test1</div>-->
+        <!--</td>-->
         
-        <td> card goes here</td>
-        </tr>
-        <tr>
-            <td>
-        <div class="player" id="player2">test2</div>
-        </td>
-        <td> card goes here</td>
-        </tr>
+        <!--<td> card goes here</td>-->
+        <!--</tr>-->
+        <!--<tr>-->
+        <!--    <td>-->
+        <!--<div class="player" id="player2">test2</div>-->
+        <!--</td>-->
+        <!--<td> card goes here</td>-->
+        <!--</tr>-->
         
-        <tr>
-            <td>
-        <div class="player" id="player3">test3</div>
-        </td>
+        <!--<tr>-->
+        <!--    <td>-->
+        <!--<div class="player" id="player3">test3</div>-->
+        <!--</td>-->
         
-        <td> card goes here</td>
-        </tr>
-        <tr>
-            <td>
-        <div class="player" id="player4">test4</div>
-        </td>
+        <!--<td> card goes here</td>-->
+        <!--</tr>-->
+        <!--<tr>-->
+        <!--    <td>-->
+        <!--<div class="player" id="player4">test4</div>-->
+        <!--</td>-->
         
-        <td> card goes here</td>
-        </tr>
-        </table>
+        <!--<td> card goes here</td>-->
+        <!--</tr>-->
+        <!--</table>-->
         <?php
         include 'functions.php';
         playerToll();
